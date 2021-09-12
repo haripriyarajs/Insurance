0857d7cd8e9ea0de9fbf071fc609efe709c464a6
haripriyarajs <haripriyarajs@gmail.com>
2021-09-12 23:40:11 +1000

    Code cleanup

--- FrontEnd/insurance-app/src/app/api/insurance-details.service.ts
+++ FrontEnd/insurance-app/src/app/api/insurance-details.service.ts
@@ -13,11 +13,8 @@ export class InsuranceDetailsService {
   response: string = '';
   responseheaders: HttpHeaders = new HttpHeaders().append('responseType', 'json').append('Access-Control-Allow-Origin', 'http://localhost:4200')
     .append('Access-Control-Allow-Credentials', 'true');
-  // formData: InsuranceDetails = new InsuranceDetails();
   calculatePremium(age:any,rating:any,coverAmount:any) {
     return this.http.get<string>(this.baseURL+`${age}/${rating}/${coverAmount}`, {headers: this.responseheaders})
-      // .toPromise()
-      // .then(res => this.response = res as string)
       .pipe(
         retry(3), // retry a failed request up to 3 times
         catchError(this.handleError) // then handle the error
@@ -28,15 +25,11 @@ export class InsuranceDetailsService {
   }
   private handleError(error: HttpErrorResponse) {
     if (error.status === 0) {
-      // A client-side or network error occurred. Handle it accordingly.
       console.error('An error occurred:', error.error);
     } else {
-      // The backend returned an unsuccessful response code.
-      // The response body may contain clues as to what went wrong.
       console.error(
         `Backend returned code ${error.status}, body was: `, error.error);
     }
-    // Return an observable with a user-facing error message.
     return throwError(
       'Something bad happened; please try again later.');
   }
--- FrontEnd/insurance-app/src/app/insurance-details/insurance-details.component.ts
+++ FrontEnd/insurance-app/src/app/insurance-details/insurance-details.component.ts
@@ -32,20 +32,16 @@ export class InsuranceDetailsComponent implements OnInit {
   defaultOccupation : any; //created for clearing occupation
   ngOnInit(): void {
     this.oService.getOccupations().subscribe((result) => {
-      console.log(result);
       this.occupations = result;
-      console.log(this.occupations);
     })
   }
   occupationChange(value: any) {
     this.occupationRating = value;
     this.showClear = true;
-   this.calculatePremium();
-    console.warn(value);
+    this.calculatePremium();
   }
 
   sumInsuredChange(value: any) {
-    console.log(value.value);
     this.sumInsured = value.value;
     this.showClear = true;
     this.calculatePremium();
@@ -54,9 +50,7 @@ export class InsuranceDetailsComponent implements OnInit {
   calculatePremium() {
     if (this.occupationRating != undefined && this.age > 0 && this.sumInsured > 0) {
       this.oService.calculatePremium(this.age,this.occupationRating,this.sumInsured).subscribe((result) => {
-        console.log(result);
         this.monthlyPremium =Number(result).toPrecision(4).toString() +" AUD";
-        console.log(this.occupations);
       })
       
     }
@@ -64,10 +58,11 @@ export class InsuranceDetailsComponent implements OnInit {
 
   clearButton(){
     this.showClear = false;
-    this.dateOfBirth = ' ';
+    this.dateOfBirth = Date.now;
     this.age =0;
     this.sumInsured = 0;
     this.defaultOccupation = '';
+    this.monthlyPremium = '';
     if(this.nameChange != undefined){
       this.nameControl.target.value = ''
      }
@@ -76,7 +71,6 @@ export class InsuranceDetailsComponent implements OnInit {
   nameChange(event: any){
     this.nameControl = event;
     this.showClear = true;
-    console.warn(event.target.value);
   }
 
   calculateAge(event : any){
@@ -95,10 +89,5 @@ export class InsuranceDetailsComponent implements OnInit {
   formatSumInsuredSliderLabel(value: number) {
     return value + "k";
   }
-
-  formatAgeSliderLabel(value: number) {
-    return value + " Y";
-  }
-
 
 }
